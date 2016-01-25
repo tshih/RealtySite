819b5d1d15f3218f9c77922359534541a091e3e6
Timothy Shih <timothy.ts.shih@gmail.com>
2016-01-25 01:06:29 -0600

    Initial Models

--- ShihsRealty/ShihsRealty/urls.py
+++ ShihsRealty/ShihsRealty/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'', include('srFrontend.urls')),
     url(r'^admin/', admin.site.urls),
+    
 
 ]
--- ShihsRealty/srFrontend/models.py
+++ ShihsRealty/srFrontend/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class TextField(models.Model):
+class WebTextField(models.Model):
 	question_text = models.TextField()
 	pub_date = models.DateTimeField('date published')
+
+#class WebPage(models.Model):
