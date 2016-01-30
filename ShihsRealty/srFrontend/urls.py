942fa97cef56fa033bed64d0f2632a69c18d5bc3
Timothy Shih <timothy.ts.shih@gmail.com>
2016-01-29 19:19:19 -0600

    Adding static files from template, added basic dynamic template and view functionality

--- ShihsRealty/srFrontend/urls.py
+++ ShihsRealty/srFrontend/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name="srFrontend"
 urlpatterns= [
 	url(r'^$', views.index, name='index'),
-	url(r'^index.html$', views.index, name='index2')
+	# url(r'^index.html$', views.index, name='index2')
 ]
--- ShihsRealty/srFrontend/views.py
+++ ShihsRealty/srFrontend/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import WebTextField
+from .models import WebPage
 
 # Create your views here.
 def index(request):
-	return render(request, 'srFrontend/index.html')
+
+	indexTextFields = dict(WebTextField.objects.filter(web_Page__page_Name="Index").values_list('text_Field_Name', 'text'))
+	return render(request, 'srFrontend/index.html',indexTextFields)
