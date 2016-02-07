18b600bddd7732b306bef67d6452320295c93bc5
Timothy Shih <timothy.ts.shih@gmail.com>
2016-02-06 22:07:13 -0600

    Added Image model and migration

--- ShihsRealty/srFrontend/models.py
+++ ShihsRealty/srFrontend/models.py
@@ -15,3 +15,21 @@ class WebTextField(models.Model):
 	web_Page = models.ManyToManyField(WebPage)
 	def __str__(self):
 		return self.text_Field_Name
+
+class Image(models.Model):
+	BACKGROUND = 0
+	CAROUSEL = 1
+	STANDARD = 2
+
+	IMAGE_TYPE = (
+		(BACKGROUND, 'Background'),
+		(CAROUSEL, 'Carousel'),
+		(STANDARD, 'Standard'),
+		)
+
+	image_Name = models.CharField(max_length=200)
+	image_Path = models.CharField(max_length=256)
+	image_Type = models.IntegerField(choices=IMAGE_TYPE)
+
+	def __str__(self):
+		return self.image_Name
